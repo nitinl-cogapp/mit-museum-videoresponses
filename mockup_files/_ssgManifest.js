17425d2eaeb51ad89e7653f374a76a0798cf8812
NitinL <nitinl@cogapp.com>
2022-01-20 13:22:08 +0000

    Add content

---
+++ mockup_files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002F","\u002F[...sitePage]","\u002Fannouncements\u002F[slug]","\u002Fexhibitions\u002F[slug]","\u002Fprograms\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
